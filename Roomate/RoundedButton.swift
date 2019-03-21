f55e66aa27d17e4417e5e727e56355b3b7ad753a
derek <derekli1995@outlook.com>
2019-03-21 01:26:32 -0400

    added tab bar 4

---
+++ Roomate/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Roomate
+//
+//  Created by Derek on 2019-03-20.
+//  Copyright © 2019 Yufeng Li. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+            self.layer.masksToBounds = true
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
---
+++ Roomate/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  Roomate
+//
+//  Created by Derek on 2019-03-20.
+//  Copyright © 2019 Yufeng Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController: UITabBarController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
