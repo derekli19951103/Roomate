4c9943b6bcb3307ceb0613cc58532e194c735c3d
leebang <leebang31698@gmail.com>
2019-03-29 17:14:00 -0400

    test con

--- Roomate/InformationController.swift
+++ Roomate/InformationController.swift
@@ -6,4 +6,43 @@
 //  Copyright © 2019 Yufeng Li. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+class InformationController: UIViewController {
+    
+    @IBOutlet weak var firstname: UITextField!
+    
+    @IBOutlet weak var lastname: UITextField!
+    
+    @IBOutlet weak var email: UITextField!
+    
+    
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var passwordAgain: UITextField!
+    
+    @IBOutlet weak var telephone: UITextField!
+    
+    @IBOutlet weak var birth: UITextField!
+    
+    @IBAction func submitForm(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        var DestViewController : TestController = segue.destination as! TestController
+//    }
+    
+    
+    
+    
+    
+}
+
+
+
+
--- Roomate/TestController.swift
+++ Roomate/TestController.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import UIKit
+
+class TestController: UIViewController {
+    
+    @IBOutlet weak var firstname: UILabel!
+    
+    @IBOutlet weak var lastname: UILabel!
+    
+    @IBOutlet weak var email: UILabel!
+    
+    @IBOutlet weak var password: UILabel!
+    
+    @IBOutlet weak var passwordAgain: UILabel!
+    
+    @IBOutlet weak var telephone: UILabel!
+    
+    @IBOutlet weak var birth: UILabel!
+    
+    var firstnameText = String()
+    var lastnameText = String()
+    var emailText = String()
+    var passwordText = String()
+    var passwordAgainText = String()
+    var telephoneText = String()
+    var birthText = String()
+    
+    
+    
+    override func viewDidLoad() {
+        
+    }
+}
